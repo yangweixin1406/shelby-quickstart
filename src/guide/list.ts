e0c38f393e3bb5fe2e7028a25bf44c53dc34e948
Akasha Thorne <akasha@shelby.xyz>
2025-10-09 21:40:45 -0700

    (wip) Refactor list guide for new SDK and catch 401 errors

--- src/guide/list.ts
+++ src/guide/list.ts
@@ -1,48 +1,42 @@
 import "dotenv/config"
 
-import { AccountAddress, Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk"
-import { input } from "@inquirer/prompts"
+import {
+	Account,
+	AccountAddress,
+	Ed25519PrivateKey,
+	Network,
+} from "@aptos-labs/ts-sdk"
 import { ShelbyNodeClient } from "@shelby-protocol/sdk/node"
 import chalk from "chalk"
 import { filesize } from "filesize"
 import { cmd } from "./util/format"
 import truncate from "./util/truncate"
 
-const SHELBY_NETWORK = process.env.SHELBY_NETWORK || "devnet"
 const SHELBY_ACCOUNT_ADDRESS = process.env.SHELBY_ACCOUNT_ADDRESS
-const SHELBY_RPC = process.env.SHELBY_RPC
-
-if (!SHELBY_RPC) {
-	console.error("SHELBY_RPC is not set in", chalk.cyan(".env"))
-	process.exit(1)
-}
+const SHELBY_ACCOUNT_PRIVATE_KEY = process.env.SHELBY_ACCOUNT_PRIVATE_KEY
 
 if (!SHELBY_ACCOUNT_ADDRESS) {
 	console.error("SHELBY_ACCOUNT_ADDRESS is not set in", chalk.cyan(".env"))
 	process.exit(1)
 }
 
-const aptos = new Aptos(
-	new AptosConfig({
-		network: Network[SHELBY_NETWORK as keyof typeof Network],
-	}),
-)
-const client = new ShelbyNodeClient({ aptos, shelby: { baseUrl: SHELBY_RPC } })
+const client = new ShelbyNodeClient({
+	network: Network.SHELBYNET,
+	apiKey: "AG-5Y2LDN4FNNRETSQRMS9VQRFFOKVHSRZ6J",
+})
 
 async function main() {
 	try {
-		const accountStr = await input({
-			message: "Which account's blobs would you like to list?",
-			default: SHELBY_ACCOUNT_ADDRESS as string,
-		})
-		if (!accountStr) {
-			console.error(
-				chalk.bold.whiteBright("No account provided. Exiting."),
-			)
-			process.exit(1)
-		}
+		console.log(
+			chalk.bold.whiteBright(
+				"Listing blobs for account",
+				`${chalk.cyan(truncate(SHELBY_ACCOUNT_ADDRESS as string))}:`,
+			),
+		)
 		const blobs = await client.coordination.getAccountBlobs({
-			account: AccountAddress.fromString(accountStr),
+			account: AccountAddress.fromString(
+				SHELBY_ACCOUNT_ADDRESS as string,
+			),
 		})
 		console.log(
 			chalk.bold.whiteBright(
@@ -75,6 +69,16 @@ async function main() {
 					"Configuration canceled. No file written.",
 				),
 			)
+			return
+		}
+		if (e instanceof Error && e.message.includes("401")) {
+			// FIXME: Add link to the docs about obtaining an API key
+			console.error(
+				chalk.bold.redBright(
+					"Unauthorized (401). Did you set your API key?",
+				),
+			)
+			console.log(e)
 		} else {
 			console.error("Unexpected error:", e)
 			console.error("---")
